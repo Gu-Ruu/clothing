19ba7edc90cb803b779486374af0dc15a2fc5c24
Gu-Ruu <anuragverma161@gmail.com>
2024-04-21 18:35:57 +0530

    tiny changes

--- src/routes/Shop/shop.component.jsx
+++ src/routes/Shop/shop.component.jsx
@@ -1,23 +1,12 @@
-import { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../Contexts/categories.cotext";
-import ProductCard from "../../components/product-card/Products-card.component";
-import './shop.styles.scss'
+import { Routes, Route } from "react-router";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import "./shop.styles.scss";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="product-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />}></Route>
+    </Routes>
   );
 };
 export default Shop;
+ 
