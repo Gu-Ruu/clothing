45ae168dcf8cdba2e1a1c3d3b3e3f21d719d403e
Gu-Ruu <a.vermanurag161@gmail.com>
2024-05-21 16:24:45 +0530

    setup typescript in all component, and payment mode through stripe

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import Home from "./routes/home/Home.component";
-import { useEffect } from "react";
-import { setCurrentUser } from "./store/user/user.action";
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from "./utility/Firebase/firebase.utils";
-import { Route, Routes } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/Shop/shop.component";
-import Checkout from "./routes/checkout/Checkout.component";
-import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+import { checkUserSession } from './store/user/user.action';
+
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    const unSubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-    return unSubscribe;
-  }, []);
+    dispatch(checkUserSession());
+  }, [dispatch]);
+
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="Shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
 };
+
 export default App;
